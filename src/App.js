4b792f0187b753a45fa59b1ed5036d12fe5488bf
breaker19 <sebakarp26@gmail.com>
2021-12-23 11:22:58 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Pet shop y veterinaria
+        </p>
+        <a
+          className="App-link"
+          href="https://tormenta.com.ar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Desarrollo by Sebastian Karp
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
