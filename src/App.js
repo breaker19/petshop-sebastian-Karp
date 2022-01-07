09408963fbbdc12a1e1d42dd6d3f143e6ad4fe21
breaker19 <sebakarp26@gmail.com>
2022-01-07 13:38:40 -0300

    cambios texto

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Desarrollo by Sebastian Karp test
+          Desarrollo by Sebastian Karp
         </a>
       </header>
     </div>
