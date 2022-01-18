a43e38c0f802013235c9e772b479c2869a08bb0d
breaker19 <sebakarp26@gmail.com>
2022-01-18 15:37:15 -0300

    Stock maximo

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,10 +6,16 @@ import { render } from '@testing-library/react';
 
  const Contador = () => {
 
-
+const stock= 10;
     const [itemsQty, setitemsQty] = useState(0);
+    
     const limiteNegativo = (qty) => {
         if (qty >= 0) {
+            setitemsQty(qty)
+        }
+    }
+    const limitestock = (qty) => {
+        if (qty <= stock) {
             setitemsQty(qty)
         }
     }
@@ -18,7 +24,7 @@ return (
         <h4>Prueba de botones</h4>
         <div style={{marginBottom: 10, paddingRight: 20}}>
 
-<Button style={{marginRight: 20}} onClick={() =>setitemsQty(itemsQty + 1) } variant="danger"> + </Button>
+<Button style={{marginRight: 20}} onClick={() =>limitestock(itemsQty + 1) } variant="danger"> + </Button>
 
 <span>{itemsQty}</span>
 
